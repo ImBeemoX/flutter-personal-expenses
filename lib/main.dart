591b8e8b0bdb2894a91d8f7d0f683a0d68852c16
Yichuan Wang <w369371@gmail.com>
2019-12-24 00:31:57 +1000

    add styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,19 +15,28 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List
-
   @override
   Widget build(BuildContext context) {
+    final List<Transaction> transactions = [
+      Transaction(id: "PS4", title: "PS4 Pro", amount: 399, date:DateTime.now()),
+      Transaction(id: "Switch", title: "Nintendo Switch", amount: 399.9, date:DateTime.now()),
+    ];
     return Scaffold(
       appBar: AppBar(
         title: Text("App bar"),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[Card(color: Colors.blue, child: Container(child: Text("Chart"), width: double.infinity,), elevation: 5, ),
-        Card(child: Text("List"),)]
+        Column(children: transactions.map((transcation) {
+          return Card(child: Row(children: <Widget>[Container(child: Text('\$' + transcation.amount.toString(),style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.deepOrange)), 
+          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15), decoration: BoxDecoration(border: Border.all()), padding: EdgeInsets.all(13),
+          ), Column(children: <Widget> [Text(transcation.title.toString(), style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),), Text(transcation.date.toString(), )] 
+          , crossAxisAlignment: CrossAxisAlignment.start,)
+          ],
+          ),
+          );
+        }).toList(),)]
       ),
     );
   }
